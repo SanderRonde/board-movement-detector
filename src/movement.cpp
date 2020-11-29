a50bc05645fcd46b1d4c9218587111fe3a5f8dfc
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-11-29 19:41:12 +0100

    fix bugs in sending process

--- src/movement.cpp
+++ src/movement.cpp
@@ -6,37 +6,51 @@
 #include <net.h>
 
 #define COOLDOWN_PERIOD 1000 * 10
+#define PING_TIME 100
 
 namespace Movement
 {
-	void setup()
-	{
-		pinMode(MOVEMENT_PIN, INPUT);
-	}
-
-	bool has_movement()
-	{
-		return digitalRead(MOVEMENT_PIN);
-	}
-
 	void notify_movement()
 	{
-		LOGN("Notifying of movement");
 		char *path = (char *)malloc(sizeof(char) * 40);
 		sprintf(path, "/movement/%s", NAME);
 		Net::req_auth(BASE_NAME, 80, path);
 		free(path);
 	}
 
-	unsigned long last_movement = 0;
+	void setup()
+	{
+		pinMode(IN_PIN, INPUT_PULLUP);
+		digitalWrite(IN_PIN, HIGH);
+	}
+
+	void send_movement()
+	{
+		char *path = (char *)malloc(sizeof(char) * 40);
+		sprintf(path, "/movement/%s", NAME);
+		Net::req_auth(BASE_NAME, 80, path);
+		free(path);
+	}
+
+	bool is_movement(int movement_read)
+	{
+		return movement_read > 600;
+	}
+
+	unsigned int last_run = millis();
+	unsigned int last_log = millis();
 	void loop()
 	{
-		if (has_movement())
+		if (millis() - last_run >= PING_TIME)
 		{
-			if (millis() > (last_movement + COOLDOWN_PERIOD))
+			last_run = millis();
+
+			int movement_read = analogRead(IN_PIN);
+			if (is_movement(movement_read) && millis() - last_log >= COOLDOWN_PERIOD)
 			{
-				notify_movement();
-				last_movement = millis();
+				LOGF("Logging movement %s - %d\n", NAME, movement_read);
+				send_movement();
+				last_log = millis();
 			}
 		}
 	}
