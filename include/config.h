37c7c7f5321fb4cdca855c42a36243516880b801
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-12-30 05:37:01 +0100

    add docs

--- include/config.h
+++ include/config.h
@@ -1,6 +1,12 @@
 #pragma once
 
+// When enabled, uses the digital pin (D1), when disabled,
+// uses the analog pin (A0)
 #define DIGITAL 1
+// When enabled, takes any single HIGH result read as absolute
+// truth that there is movement. When false, checks whether the
+// value holds for the configured amount of MS before signaling
+// movement
 #define ABSOLUTE_READ 1
 
 #define NAME "door"
