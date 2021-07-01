11be1c3068e355ceafbd276fbef55e33c6542e69
Sander Ronde <awsdfgvhbjn@gmail.com>
2021-07-01 21:24:33 +0200

    add logging mode

--- src/movement.cpp
+++ src/movement.cpp
@@ -1,48 +1,63 @@
 #include <movement.h>
 #include <secrets.h>
 #include <Arduino.h>
-#include <telnet.h>
+#include "lib-telnet.h"
 #include <config.h>
-#include <net.h>
+#include "lib-net.h"
 
 #define MOVEMENT_TIME 100
-#define MOVEMENT_MIN 600
 #define COOLDOWN_TIME 1000 * 3
+#define MEASUREMENT_COOLDOWN 50
+
+#define LOGGING_MODE 1
 
 namespace Movement
 {
 	void setup()
 	{
-#ifndef DIGITAL
-		pinMode(IN_PIN, INPUT_PULLUP);
-		digitalWrite(IN_PIN, HIGH);
-#else
 		pinMode(IN_PIN, INPUT);
-#endif
 	}
 
 	void send_movement()
 	{
 		char *path = (char *)malloc(sizeof(char) * 40);
 		sprintf(path, "/movement/%s", NAME);
-		Net::req_auth(BASE_NAME, 80, path);
+		Net::req_auth(SECRET_KEY, BASE_NAME, 80, path);
 		free(path);
 	}
 
+	int last_read = 0;
 	bool is_movement()
 	{
 #ifdef DIGITAL
 		return digitalRead(IN_PIN) == HIGH;
 #else
-		return analogRead(IN_PIN) > MOVEMENT_MIN;
+		int current_read = analogRead(IN_PIN);
+		#ifdef LOGGING_MODE
+		if (current_read != last_read)
+		{
+			LOGF("Distance = %d mm\n", current_read);
+			last_read = current_read;
+		}
+		#endif
+#ifdef INCREASING_READS
+		return current_read > MOVEMENT_MIN;
+#else
+		return current_read < MOVEMENT_MIN;
+#endif
 #endif
 	}
 
 	bool has_been_off = true;
 	unsigned long last_log = millis();
 	unsigned long movement_since = 0;
+	unsigned long last_measurement = millis();
 	void loop()
 	{
+		if (millis() - last_measurement <= MEASUREMENT_COOLDOWN)
+			return;
+		last_measurement = millis();
+
 		bool movement = is_movement();
 		if (movement)
 		{
